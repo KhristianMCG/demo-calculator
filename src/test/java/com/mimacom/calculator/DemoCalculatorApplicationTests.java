5bc338255915aec44ad26401513c25c509312740
cmtekprojects <cmtekprojects@gmail.com>
2023-04-18 16:08:13 +0200

    Assert tru to verify the context,not the goal in this case.

---
+++ src/test/java/com/mimacom/calculator/DemoCalculatorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mimacom.calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+}

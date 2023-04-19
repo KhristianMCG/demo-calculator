bfde16c220f4b0f58dec432ac1918eaa87e43fe0
cmtekprojects <cmtekprojects@gmail.com>
2023-04-19 16:17:08 +0200

    Adding spring fox configuration

---
+++ src/main/java/com/mimacom/configuration/SpringFoxConfiguration.java
@@ -0,0 +1,39 @@
+package com.mimacom.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(this.apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "REST API",
+                "Backend service with exposed apis.",
+                "API V1",
+                "Terms of service",
+                new Contact("Cristian Moreno", "https://www.cmtekprojects.com", "cristian.moreno@cmtekprojects.com"),
+                "License of API", "API license URL", Collections.emptyList());
+    }
+}

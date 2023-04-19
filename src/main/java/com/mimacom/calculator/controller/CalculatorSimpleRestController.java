d0c9d4e537ef861202b7eccb22b2ee44e32db7ad
cmtekprojects <cmtekprojects@gmail.com>
2023-04-19 17:08:35 +0200

    Adding tracing result of APIs

--- src/main/java/com/mimacom/calculator/controller/CalculatorSimpleRestController.java
+++ src/main/java/com/mimacom/calculator/controller/CalculatorSimpleRestController.java
@@ -2,6 +2,7 @@ package com.mimacom.calculator.controller;
 
 import com.mimacom.calculator.exception.ExceptionCalculator;
 import com.mimacom.calculator.service.CalculatorRestControllerSimpleService;
+import io.corp.calculator.TracerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,28 +14,37 @@ import java.math.BigDecimal;
 @RequestMapping("/calculator")
 public class CalculatorSimpleRestController {
 
-    public static final String APPLICATION_JSON = "application/json";
+    private static final String APPLICATION_JSON = "application/json";
+    private final TracerImpl tracer = new TracerImpl();
     @Autowired
     private CalculatorRestControllerSimpleService calculatorRestControllerSimpleService;
 
     @PostMapping(value = "/add/{valuea}/{valueb}", produces = APPLICATION_JSON)
     public ResponseEntity<BigDecimal> add(@PathVariable String valuea, @PathVariable String valueb) {
-        return new ResponseEntity<>(calculatorRestControllerSimpleService.add(valuea, valueb), HttpStatus.OK);
+        final BigDecimal result = calculatorRestControllerSimpleService.add(valuea, valueb);
+        tracer.trace(result);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @PostMapping(value = "/subtract/{valuea}/{valueb}", produces = APPLICATION_JSON)
     public ResponseEntity<BigDecimal> subtract(@PathVariable String valuea, @PathVariable String valueb) {
-        return new ResponseEntity<>(calculatorRestControllerSimpleService.subtract(valuea, valueb), HttpStatus.OK);
+        final BigDecimal result = calculatorRestControllerSimpleService.subtract(valuea, valueb);
+        tracer.trace(result);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @PostMapping(value = "/multiply/{valuea}/{valueb}", produces = APPLICATION_JSON)
     public ResponseEntity<BigDecimal> multiply(@PathVariable String valuea, @PathVariable String valueb) {
-        return new ResponseEntity<>(calculatorRestControllerSimpleService.multiply(valuea, valueb), HttpStatus.OK);
+        final BigDecimal result = calculatorRestControllerSimpleService.multiply(valuea, valueb);
+        tracer.trace(result);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @PostMapping(value = "/divide/{valuea}/{valueb}", produces = APPLICATION_JSON)
     public ResponseEntity<BigDecimal> divide(@PathVariable String valuea, @PathVariable String valueb) {
-        return new ResponseEntity<>(calculatorRestControllerSimpleService.divide(valuea, valueb), HttpStatus.OK);
+        final BigDecimal result = calculatorRestControllerSimpleService.divide(valuea, valueb);
+        tracer.trace(result);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @ExceptionHandler(value = ExceptionCalculator.class)
